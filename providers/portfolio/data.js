1ae0de6fdaaee570b48a3127a4af79247b2d61e8
Tylermutai <paris@atlassan.com>
2021-07-28 14:52:58 +0300

    [FEATURE]:Added additional portfolio items

--- providers/portfolio/data.js
+++ providers/portfolio/data.js
@@ -3,9 +3,7 @@ export const portfolio = [
         id: 1,
         name: "NPBC Portal",
         image: "/images/portfolio/npbc/npbc-portal.png",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum\n" +
-            "has been the industry's standard dummy text ever since the 1500s, when an unknown\n" +
-            "printer took a galley of type and scrambled it to make a type specimen book.",
+        description: "A school portal built for Nairobi Pentecostal Bible College",
         images: [
             '/images/portfolio/npbc/npbc-portal.png',
             '/images/portfolio/npbc/npbc_1.png',
@@ -25,16 +23,11 @@ export const portfolio = [
         id: 2,
         name: "Destiny Capsules Mobile App",
         image: "/images/portfolio/destiny_capsules/destiny-capsules.png",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum\n" +
-            "has been the industry's standard dummy text ever since the 1500s, when an unknown\n" +
-            "printer took a galley of type and scrambled it to make a type specimen book.",
+        description: "An E-commerce platform that sells books online",
         images: [
-            'https://upload.wikimedia.org/wikipedia/en/d/db/RWS_Tarot_06_Lovers.jpg',
-            'https://upload.wikimedia.org/wikipedia/en/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg',
-            'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg',
-            '/images/portfolio/nuru_app/nuru_app.jpg',
-            '/images/portfolio/services_app/services_app.jpg',
-            '/images/portfolio/destiny_capsules/destiny_capsules.jpg',
+            "/images/portfolio/destiny_capsules/destiny-capsules.png",
+            "/images/portfolio/destiny_capsules/destiny-capsules_1.png",
+            "/images/portfolio/destiny_capsules/destiny-capsules_2.png",
         ]
     },
 
@@ -42,16 +35,14 @@ export const portfolio = [
         id: 3,
         name: "Nuru Mobile Application",
         image: "/images/portfolio/nuru_app/nuru.png",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum\n" +
-            "has been the industry's standard dummy text ever since the 1500s, when an unknown\n" +
-            "printer took a galley of type and scrambled it to make a type specimen book.",
+        description: "A social and community application that connects different church communities",
         images: [
-            'https://upload.wikimedia.org/wikipedia/en/d/db/RWS_Tarot_06_Lovers.jpg',
-            'https://upload.wikimedia.org/wikipedia/en/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg',
-            'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg',
-            '/images/portfolio/nuru_app/nuru_app.jpg',
-            '/images/portfolio/services_app/services_app.jpg',
-            '/images/portfolio/destiny_capsules/destiny_capsules.jpg',
+            "/images/portfolio/nuru_app/nuru.png",
+            "/images/portfolio/nuru_app/nuru-1.png",
+            "/images/portfolio/nuru_app/nuru-2.png",
+            "/images/portfolio/nuru_app/nuru-3.png",
+            "/images/portfolio/nuru_app/nuru-4.png",
+            "/images/portfolio/nuru_app/nuru-5.png",
         ]
     },
 
@@ -59,16 +50,9 @@ export const portfolio = [
         id: 4,
         name: "E-Commerce App",
         image: "/images/portfolio/e-commerce-app/e-commerce.png",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum\n" +
-            "has been the industry's standard dummy text ever since the 1500s, when an unknown\n" +
-            "printer took a galley of type and scrambled it to make a type specimen book.",
+        description: "An all purpose e-commerce application that facilitates sending of general products online",
         images: [
-            'https://upload.wikimedia.org/wikipedia/en/d/db/RWS_Tarot_06_Lovers.jpg',
-            'https://upload.wikimedia.org/wikipedia/en/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg',
-            'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg',
-            '/images/portfolio/nuru_app/nuru_app.jpg',
-            '/images/portfolio/services_app/services_app.jpg',
-            '/images/portfolio/destiny_capsules/destiny_capsules.jpg',
+            "/images/portfolio/e-commerce-app/e-commerce.png",
         ]
     },
 
@@ -76,42 +60,16 @@ export const portfolio = [
         id: 5,
         name: "Services App",
         image: "/images/portfolio/services_app/services-app.png",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum\n" +
-            "has been the industry's standard dummy text ever since the 1500s, when an unknown\n" +
-            "printer took a galley of type and scrambled it to make a type specimen book.",
+        description: "A mobile application that facilitates connecting users with specialists",
         images: [
-            'https://upload.wikimedia.org/wikipedia/en/d/db/RWS_Tarot_06_Lovers.jpg',
-            'https://upload.wikimedia.org/wikipedia/en/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg',
-            'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg',
-            '/images/portfolio/nuru_app/nuru_app.jpg',
-            '/images/portfolio/services_app/services_app.jpg',
-            '/images/portfolio/destiny_capsules/destiny_capsules.jpg',
-        ]
-    },
-
-    {
-        id: 6,
-        name: "Kenlan Calculators",
-        image: "/images/portfolio/lady-shutterbug.jpg",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum\n" +
-            "has been the industry's standard dummy text ever since the 1500s, when an unknown\n" +
-            "printer took a galley of type and scrambled it to make a type specimen book.",
-        images: [
-            'https://upload.wikimedia.org/wikipedia/en/d/db/RWS_Tarot_06_Lovers.jpg',
-            'https://upload.wikimedia.org/wikipedia/en/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg',
-            'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg',
-            '/images/portfolio/nuru_app/nuru_app.jpg',
-            '/images/portfolio/services_app/services_app.jpg',
-            '/images/portfolio/destiny_capsules/destiny_capsules.jpg',
+            "/images/portfolio/services_app/services-app.png",
         ]
     },
     {
         id: 7,
         name: "Supermarket App",
         image: "/images/portfolio/supermarket_app/supermarket-app.png",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum\n" +
-            "has been the industry's standard dummy text ever since the 1500s, when an unknown\n" +
-            "printer took a galley of type and scrambled it to make a type specimen book.",
+        description: "A mobile application that enables payment of goods in a supermarket by scanning a barcode, and includes purchase statistics",
         images: [
             'https://upload.wikimedia.org/wikipedia/en/d/db/RWS_Tarot_06_Lovers.jpg',
             'https://upload.wikimedia.org/wikipedia/en/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg',

42fd1080b5d6dace8cb9a04b44e0f5f6820b48ac
SAMUEL <peter16muhia@gmail.com>
2021-07-30 07:31:20 +0300

    [FEATURE]:SEO Adjustments

--- components/main/layout.js
+++ components/main/layout.js
@@ -23,7 +23,6 @@ function Layout({
                 <meta property="og:title" content="Software Development Company in Kenya |  We are a software development company based in Nairobi, Kenya dedicated to providing you with a seamless transition into the digital experience in Kenya and Beyond the region." />
 
                 <meta property="og:description" content="We are a software development company in Nairobi Kenya. Contact us for Mobile Application Development, E-commerce systems, E-learning systems, Management system, Websites, USSD Codes, Bulky SMS, Short Codes, SEO (Search Engine Optimizarion)" />
-  
                 <meta name="keywords" content="Software Development Company Nairobi - Kenya, Mobile Application Development, Mobile Apps Developers in Kenya, E-commerce systems in Kenya, E-learning systems in Kenya, Management system Kenya, Websites, USSD Codes in Kenya, Bulky SMS, Short Codes in Kenya, SEO (Search Engine Optimizarion) Company in Nairobi - Kenya" />
             </Head>
             <Header/>

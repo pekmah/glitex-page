c4c3549a16179186ab7c3f6ca68bdfbd10aeba7b
Pekmah <ericpekmah@gmail.com>
2023-04-04 15:06:13 +0300

    feat: Mobile-applications-developers-in-ethiopia

--- components/pages/mobile/AreYouInSearch.js
+++ components/pages/mobile/AreYouInSearch.js
@@ -1,50 +1,68 @@
 import React from "react";
 import Button from "../../general/Button";
+import Ethiopia from "./icons/Ethiopia";
 
-const AreYouInSearch = () => {
-  return (
-    <div className="flex flex-col md:flex-row py-8 lg:py-12 bg-white">
-      {/* image div */}
+const AreYouInSearch = ({showBtn}) => {
+    return (
+        <div className={"bg-white pb-5"}>
+            <div className="flex flex-col md:flex-row py-8 lg:py-12 ">
+                {/* image div */}
 
-      {/* text description div */}
-      <div className="flex flex-col gap-6 flex-1 justify-center items-center">
-        <div className="flex flex-col w-[85%] gap-4 md:gap-8">
-          <h5 className=" text-2xl md:text-3xl font-medium leding-[50px]">
-            Are you in search of the mobile app developers in Kenya?
-          </h5>
-          {/* body */}
-          <div className="">
-            <p className="text-base sm:text-lg">
-              You are then in the right place. Glitex Solutions Limited is a
-              renowned firm that develops both iOS and Android mobile
-              applications. At Glitex Solutions Limited, you not only get mobile
-              application development services but also the right advises on how
-              to implement your app and how to roll it out.
-            </p>
-            <br />
-            <p className="text-base sm:text-lg">
-              We have served many countries in the Africa continent and beyond
-              the continent. We have our headquarters in Nairobi, Kenya. Contact
-              us today.
-            </p>
-          </div>
-          <div className="flex">
-            <Button
-              className={"bg-primary w-48 text-secondary text-lg"}
-              text={"Call us Today!"}
-            />
-          </div>
+                {/* text description div */}
+                <div className="flex flex-col gap-6 flex-1 justify-center items-center">
+                    <div className="flex flex-col w-[85%] gap-4 md:gap-8">
+                        <h5 className=" text-2xl md:text-3xl font-medium leding-[50px]">
+                            Are you in search of the mobile app developers in Kenya?
+                        </h5>
+                        {/* body */}
+                        <div className="">
+                            <p className="text-base sm:text-lg">
+                                You are then in the right place. Glitex Solutions Limited is a
+                                renowned firm that develops both iOS and Android mobile
+                                applications. At Glitex Solutions Limited, you not only get mobile
+                                application development services but also the right advises on how
+                                to implement your app and how to roll it out.
+                            </p>
+                            <br/>
+                            <p className="text-base sm:text-lg">
+                                We have served many countries in the Africa continent and beyond
+                                the continent. We have our headquarters in Nairobi, Kenya. Contact
+                                us today.
+                            </p>
+                        </div>
+                        <div className="flex">
+                            <Button
+                                className={"bg-primary w-48 text-secondary text-lg"}
+                                text={"Call us Today!"}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="flex items-center justify-center flex-1 p-4 lg:p-0">
+                    <img
+                        src="/images/portfolio/portfolio.png"
+                        alt="__"
+                        className="max-h-[595px] w-full object-cover h-auto"
+                    />
+                </div>
+
+            </div>
+            {/*  Available in Ethiopia  */}
+            {showBtn && <a href="/mobile/ethiopia">
+                <div
+                    className={"bg-[#FFC53F54] text-xl font-bold rounded-full px-14 h-10 flex items-center justify-center w-1/2 mx-auto gap-3 my-5"}>
+
+                    Hey! we are also live in <span className={"text-primary"}> Ethiopia </span>
+                    {/*    Ethiopia flag*/}
+                    <span>
+                        <Ethiopia/>
+                    </span>
+
+                </div>
+            </a>}
+
         </div>
-      </div>
-      <div className="flex items-center justify-center flex-1 p-4 lg:p-0">
-        <img
-          src="/images/portfolio/portfolio.png"
-          alt="__"
-          className="max-h-[595px] w-full object-cover h-auto"
-        />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default AreYouInSearch;
---
+++ components/pages/mobile/ethiopia/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div
+            className={
+                " px-2 md:px-8 lg:px-12 py-8 bg-primary text-center flex flex-col justify-center items-center"
+            }
+        >
+            <h3 className="lg:text-3xl sm:text-xl text-lg lg:w-10/12 mx-auto font-bold mb-3 ">
+                Mobile Application Developers in Ethiopia | Mobile application
+                developers in Ethiopia
+            </h3>
+
+            <p className="text-lg lg:w-2/3 w-[95%] mt-3 lg:mt-2">
+                Are you searching for mobile application developers in Ethiopia? Look no more.
+                Glitex Solutions Limited is a Kenya company that develops mobile applications for businesses in Ethiopia
+                and across the region.
+
+            </p>
+
+        </div>
+    );
+};
+
+export default About;

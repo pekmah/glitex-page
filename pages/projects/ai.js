1553cbe5c5a4b007112927ad2a6e1f780f0a0961
Pekmah <pekstaar@gmail.com>
2023-05-26 08:34:31 +0300

    refactor: title update

--- pages/projects/ai.js
+++ pages/projects/ai.js
@@ -1,41 +1,41 @@
-import React from 'react';
+import React from "react";
 import Layout from "../../components/main/layout";
 import Carousel from "../../components/general/Carousel";
 import Footer from "../../components/pages/home/footer";
-import About from '../../components/pages/projects/ai/About';
-import Portfolio from '../../components/pages/projects/ai/Portfolio';
-import OurFocus from '../../components/pages/projects/ai/OurFocus';
-import Benefits from '../../components/pages/projects/ai/Benefits';
+import About from "../../components/pages/projects/ai/About";
+import Portfolio from "../../components/pages/projects/ai/Portfolio";
+import OurFocus from "../../components/pages/projects/ai/OurFocus";
+import Benefits from "../../components/pages/projects/ai/Benefits";
 
 const Ai = () => {
-    return (
-        <Layout>
-            <Carousel
-                title={
-                    <div className="text-center leading-8 lg:leading-[50px] mt-5 md:mt-0">
-                        <p className={"w-[80%] mx-auto mt-12"}>AI and ML (Artificial Intelligence & Machine learning)
-                            Developers in
-                            Nairobi
-                            Kenya | Glitex Solutions Limited </p>
-                    </div>
-                }
-            />
+  return (
+    <Layout>
+      <Carousel
+        title={
+          <div className="text-center leading-8 lg:leading-[50px] mt-5 md:mt-0">
+            <p className={"w-[80%] mx-auto mt-12"}>
+              Ai and ML (Artificial Intelligence & Machine learning) Developers
+              in Nairobi Kenya
+            </p>
+          </div>
+        }
+      />
 
-            {/* About */}
-            <About/>
+      {/* About */}
+      <About />
 
-            {/* AI portfolio... */}
-            <Portfolio/>
+      {/* AI portfolio... */}
+      <Portfolio />
 
-            {/* Our Focus .... */}
-            <OurFocus />
+      {/* Our Focus .... */}
+      <OurFocus />
 
-            {/* what sets us apart */}
-            <Benefits />
+      {/* what sets us apart */}
+      <Benefits />
 
-            <Footer/>
-        </Layout>
-    );
+      <Footer />
+    </Layout>
+  );
 };
 
 export default Ai;

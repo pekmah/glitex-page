d95caec0a2dd1ce81ed6688d9efd3278c40f1045
Pekmah <pekstaar@gmail.com>
2023-05-26 04:59:49 +0300

    chore: div converted to section, header added

--- components/pages/home/AboutUs.js
+++ components/pages/home/AboutUs.js
@@ -2,17 +2,24 @@ import React from "react";
 
 const AboutUs = () => {
   return (
-    <div
+    <section
       className={
         "min-h-[100vh] bg-primary text-center flex flex-col py-8 md:py-0 md:justify-center items-center"
       }
+      id={"about"}
     >
-      <h3 className="lg:text-5xl sm:text-3xl text-xl font-bold lg:leading-[60px] sm:leading-[50px] leading-10" data-aos="fade-up">
+      <h3
+        className="lg:text-5xl sm:text-3xl text-xl font-bold lg:leading-[60px] sm:leading-[50px] leading-10"
+        data-aos="fade-up"
+      >
         Software development company <br />
         in Kenya | Glitex Solutions Limited
       </h3>
 
-      <p className="md:my-10 my-3 lg:text-2xl lg:w-10/12 sm:text-xl text-lg " data-aos="fade-up">
+      <p
+        className="md:my-10 my-3 lg:text-2xl lg:w-10/12 sm:text-xl text-lg "
+        data-aos="fade-up"
+      >
         Glitex Solutions Limited is a tech-solutions company built by a group of
         visionary young and passionate individuals, dedicated to help our
         clients continuously create value for their customers.
@@ -24,15 +31,14 @@ const AboutUs = () => {
         leveraging modern day tech stacks.
       </p>
 
-      <div className="flex gap-3 sm:gap-5 lg:gap-8 font-semibold about-stats" data-aos="fade-up">
+      <div
+        className="flex gap-3 sm:gap-5 lg:gap-8 font-semibold about-stats"
+        data-aos="fade-up"
+      >
         <div className="text-2xl lg:text-4xl sm:text-3xl font-bold">
           <div className="flex justify-center">
-            <div className="stats__count">
-              67
-            </div>
-            <div className="stats__countt">
-              +
-            </div>
+            <div className="stats__count">67</div>
+            <div className="stats__countt">+</div>
           </div>
           {/* <span className="text-2xl lg:text-4xl sm:text-3xl font-bold tracking-wider">
             67 +
@@ -46,12 +52,8 @@ const AboutUs = () => {
         </span>
         <div className="text-2xl lg:text-4xl sm:text-3xl font-bold">
           <div className="flex justify-center">
-            <div className="stats__count">
-              113
-            </div>
-            <div className="stats__countt">
-              +
-            </div>
+            <div className="stats__count">113</div>
+            <div className="stats__countt">+</div>
           </div>
           {/* <span className="text-2xl lg:text-4xl sm:text-3xl font-bold tracking-wider">
             113 +
@@ -59,7 +61,7 @@ const AboutUs = () => {
           <p className="md:text-md text-base font-semibold">Happy clients</p>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 

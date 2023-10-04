a5a10de18f355fd08b401a1771243f9c429ee527
glitexsolutions <87663231+glitexsolutions@users.noreply.github.com>
2023-10-04 19:20:34 +0300

    Update BestMobileDevs.js

--- components/pages/mobile/BestMobileDevs.js
+++ components/pages/mobile/BestMobileDevs.js
@@ -53,7 +53,7 @@ const BestMobileDevs = () => {
                         <img
                             src={img}
                             alt="__"
-                            className="max-h-[600px] w-auto h-auto rounded-xl"
+                            className="max-h-[650px] w-auto h-auto rounded-xl"
                         />
                     ))}
                 </div>
@@ -63,7 +63,7 @@ const BestMobileDevs = () => {
                     <img
                         src={"/images/portfolio/joosmoo-1.webp"}
                         alt="__"
-                        className="max-h-[600px] object-contain w-[100%] md:h-auto rounded-xl mt-4"
+                        className="max-h-[650px] object-contain w-[100%] md:h-auto rounded-xl mt-4"
                     />
 
                 </div>

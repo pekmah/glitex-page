72d421a28ddd93f4399ba23ca54feccda985e8b2
pekmah <ericpekmah@gmail.com>
2023-10-10 06:59:35 +0300

    feat(#12): classname made dynamic

--- components/main/layout.js
+++ components/main/layout.js
@@ -3,11 +3,11 @@ import SideNav from "../general/SideNav";
 import Header from "./header";
 import WhatsappBtn from "../general/WhatsappBtn";
 
-function Layout({children, title, backgroundColor}) {
+function Layout({children,className, title, backgroundColor}) {
     const [showNav, setShowNav] = useState(false);
 
     return (
-        <div className="bg-secondary min-h-screen max-w-screen overflow-hidden">
+        <div className={`bg-secondary min-h-screen max-w-screen overflow-hidden ${className}`}>
             <Header showNav={showNav} handleOpenNav={() => setShowNav(true)}/>
             <SideNav isOpen={showNav} handleCloseNav={() => setShowNav(false)}/>
             {children}

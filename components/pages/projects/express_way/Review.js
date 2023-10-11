00a68d8c113d717c468950afc88965478f3b5f13
pekmah <ericpekmah@gmail.com>
2023-10-11 12:23:23 +0300

    feat(#12): form iframe added

--- components/pages/projects/express_way/Review.js
+++ components/pages/projects/express_way/Review.js
@@ -6,6 +6,16 @@ const Review = () => {
       <h5 className={"text-2xl font-semibold text-black mb-5"}>
         Leave a Review
       </h5>
+
+      <div>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSfuUmDdhgvT4UxKzEwF4rkmLHoqseO-pgyhH-I5jSQtID23SQ/viewform?embedded=true"
+          className={"w-[350px] md:w-[650px]"}
+          height="700"
+        >
+          Loading…
+        </iframe>
+      </div>
     </div>
   );
 };

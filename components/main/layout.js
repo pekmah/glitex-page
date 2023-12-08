606197dde5c2a0178607c3444cae88d9afbffb86
Pekmah <ericpekmah@gmail.com>
2023-12-08 09:05:37 +0300

    feat(#18): white bg added

--- components/main/layout.js
+++ components/main/layout.js
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import SideNav from "../general/SideNav";
 import Header from "./header";
-import WhatsappBtn from "../general/WhatsappBtn";
 
 function Layout({ children, className, hidePhone }) {
   const [showNav, setShowNav] = useState(false);
 
   return (
     <div
-      className={`bg-secondary min-h-screen max-w-screen overflow-hidden ${className}`}
+      className={`bg-white min-h-screen max-w-screen overflow-hidden ${className}`}
     >
       <Header
         showNav={showNav}
@@ -18,7 +17,7 @@ function Layout({ children, className, hidePhone }) {
       <SideNav isOpen={showNav} handleCloseNav={() => setShowNav(false)} />
       {children}
 
-      <WhatsappBtn />
+      {/*<WhatsappBtn />*/}
     </div>
   );
 }

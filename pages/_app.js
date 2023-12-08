f9650ce0f5c497f98e1c8b4f4bdb56aeca4a0eba
Pekmah <ericpekmah@gmail.com>
2023-12-08 09:07:00 +0300

    refactor(#18): mixpanel disabled

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,24 @@
-import '../styles/globals.scss'
-import Head from 'next/head';
-import {useEffect} from "react";
-import mixpanel from 'mixpanel-browser'
+import "../styles/globals.scss";
+import Head from "next/head";
 
-function MyApp({Component, pageProps}) {
-    useEffect(() => {
-        mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_ID||'',{ debug: true, track_pageview: true, persistence: 'localStorage' })
-        mixpanel.track_pageview();
-    }, []);
+function MyApp({ Component, pageProps }) {
+  // useEffect(() => {
+  //     mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_ID||'',{ debug: true, track_pageview: true, persistence: 'localStorage' })
+  //     mixpanel.track_pageview();
+  // }, []);
 
-    return (
-        <>
-            <Head>
-                <meta charSet="utf-8"/>
-                <meta name="description" content=""/>
-                <meta name="author" content=""/>
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
 
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            </Head>
-            <Component {...pageProps} />
-        </>
-
-    )
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;

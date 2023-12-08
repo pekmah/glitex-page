f91b4c8052214524967bca8f5a300cdb8010aa94
Pekmah <ericpekmah@gmail.com>
2023-12-08 09:06:46 +0300

    refactor(#18): unused imports deleted

--- pages/_document.js
+++ pages/_document.js
@@ -1,100 +1,96 @@
-import Document, {Head, Html, Main, NextScript} from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <link rel="stylesheet" href="/css/vendor.css"/>
-                    <link rel="stylesheet" href="/css/base.css/"/>
-                    <link rel="stylesheet" href="/css/custom.css"/>
-                    <link rel="stylesheet" href="/css/main.css"/>
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="stylesheet" href="/css/vendor.css" />
+          <link rel="stylesheet" href="/css/main.css" />
 
-                    <script src="/js/modernizr.js"/>
-                    <script src="/js/pace.min.js"/>
-                    {/*<link rel="shortcut icon" href="logo.png" type="image/x-icon"/>*/}
-                    {/*<link rel="icon" href="logo.png" type="image/x-icon"/>*/}
-                </Head>
-                <body id="top">
-                <Main/>
-                <NextScript/>
-                <script src="/js/jquery-3.2.1.min.js"/>
-                <script src="/js/plugins.js"/>
-                <script src="/js/main.js"/>
-                {/*Scripts etc.*/}
-                <div aria-hidden="true" className="pswp" role="dialog" tabIndex="-1">
-                    <div className="pswp__bg h-0"/>
-                    <div className="pswp__scroll-wrap">
-                        <div className="pswp__container">
-                            <div className="pswp__item"/>
-                            <div className="pswp__item"/>
-                            <div className="pswp__item"/>
-                        </div>
-                        <div className="pswp__ui pswp__ui--hidden">
-                            <div className="pswp__top-bar">
-                                <div className="pswp__counter"/>
-                                <button
-                                    className="pswp__button pswp__button--close"
-                                    title="Close (Esc)"
-                                />
-                                <button
-                                    className="pswp__button pswp__button--share"
-                                    title="Share"
-                                />
-                                <button
-                                    className="pswp__button pswp__button--fs"
-                                    title="Toggle fullscreen"
-                                />
-                                <button
-                                    className="pswp__button pswp__button--zoom"
-                                    title="Zoom in/out"
-                                />
-                                <div className="pswp__preloader">
-                                    <div className="pswp__preloader__icn">
-                                        <div className="pswp__preloader__cut">
-                                            <div className="pswp__preloader__donut"/>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
-                                <div className="pswp__share-tooltip"/>
-                            </div>
-                            <button
-                                className="pswp__button pswp__button--arrow--left"
-                                title="Previous (arrow left)"
-                            />
-                            <button
-                                className="pswp__button pswp__button--arrow--right"
-                                title="Next (arrow right)"
-                            />
-                            <div className="pswp__caption">
-                                <div className="pswp__caption__center"/>
-                            </div>
-                        </div>
+          {/*<link rel="shortcut icon" href="logo.png" type="image/x-icon"/>*/}
+          {/*<link rel="icon" href="logo.png" type="image/x-icon"/>*/}
+        </Head>
+        <body id="top">
+          <Main />
+          <NextScript />
+          <script src="/js/jquery-3.2.1.min.js" />
+          <script src="/js/plugins.js" />
+          <script src="/js/main.js" />
+          {/*Scripts etc.*/}
+          <div aria-hidden="true" className="pswp" role="dialog" tabIndex="-1">
+            <div className="pswp__bg h-0" />
+            <div className="pswp__scroll-wrap">
+              <div className="pswp__container">
+                <div className="pswp__item" />
+                <div className="pswp__item" />
+                <div className="pswp__item" />
+              </div>
+              <div className="pswp__ui pswp__ui--hidden">
+                <div className="pswp__top-bar">
+                  <div className="pswp__counter" />
+                  <button
+                    className="pswp__button pswp__button--close"
+                    title="Close (Esc)"
+                  />
+                  <button
+                    className="pswp__button pswp__button--share"
+                    title="Share"
+                  />
+                  <button
+                    className="pswp__button pswp__button--fs"
+                    title="Toggle fullscreen"
+                  />
+                  <button
+                    className="pswp__button pswp__button--zoom"
+                    title="Zoom in/out"
+                  />
+                  <div className="pswp__preloader">
+                    <div className="pswp__preloader__icn">
+                      <div className="pswp__preloader__cut">
+                        <div className="pswp__preloader__donut" />
+                      </div>
                     </div>
+                  </div>
                 </div>
-
-                <script
-                    async
-                    src="https://www.googletagmanager.com/gtag/js?id=G-6L8MC15QF8"
+                <div className="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+                  <div className="pswp__share-tooltip" />
+                </div>
+                <button
+                  className="pswp__button pswp__button--arrow--left"
+                  title="Previous (arrow left)"
                 />
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
+                <button
+                  className="pswp__button pswp__button--arrow--right"
+                  title="Next (arrow right)"
+                />
+                <div className="pswp__caption">
+                  <div className="pswp__caption__center" />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-6L8MC15QF8"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
 
                     gtag('config', 'G-6L8MC15QF8');
           `,
-                    }}
-                />
-                <script></script>
-                </body>
-            </Html>
-        );
-    }
+            }}
+          />
+          <script></script>
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;

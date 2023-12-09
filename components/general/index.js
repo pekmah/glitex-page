c5d702f38df72359ea779da920df0d4e2d14a5b2
Pekmah <ericpekmah@gmail.com>
2023-12-09 12:27:22 +0300

    refactor(#19): general components exported

---
+++ components/general/index.js
@@ -0,0 +1,6 @@
+import CButton from "./CButton";
+import CButtonOutlined from "./CButtonOutlined";
+import SideNav from "./SideNav";
+import WhatsappBtn from "./WhatsappBtn";
+
+export { CButton, CButtonOutlined, SideNav, WhatsappBtn };

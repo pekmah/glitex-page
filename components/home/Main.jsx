6f798b173b76f6300dfae95645472df4f702d87e
Pekmah <ericpekmah@gmail.com>
2023-12-10 12:04:16 +0300

    style(#20): main text and images added

--- components/home/Main.jsx
+++ components/home/Main.jsx
@@ -1,9 +1,60 @@
 import React from "react";
 
-import { SectionWrapper } from "../general";
+import { CButton, SectionWrapper } from "../general";
 
 const Main = () => {
-  return <SectionWrapper className={""}>HELOOWW</SectionWrapper>;
+  return (
+    <SectionWrapper className={"flex md:px-5 md:min-h-[65vh] "}>
+      {/*  text container   */}
+      <div className={"flex-1 center-x flex-col pl-4"}>
+        <h1 className={"md:leading-[60px] w-full md:w-4/5"}>
+          Enabling 1M+ Visionaries:
+          <br className={"hidden md:flex"} /> Your Trusted Tech Partner for
+          Shaping Tomorrow
+        </h1>
+        {/*  qualities list  */}
+        <p
+          className={
+            "font-lato text-sm md:text-2xl my-2 md:my-5 text-primary font-medium"
+          }
+        >
+          <span>Quality . </span>
+          <span>Security . </span>
+          <span>Scalable</span>
+        </p>
+        {/*  buttons  */}
+        <div className={"center-y-between-x gap-5 mr-auto mt-1 md:mt-5"}>
+          <CButton
+            className={"px-6 text-xs md:text-base"}
+            text={"Request Quote"}
+          />
+          <CButton
+            text={"Call: +254 707 021 821"}
+            className={
+              "bg-white btn-shadow hover:btn-shadow-deep  duration-300 hover:scale-105 hidden md:flex text-primary"
+            }
+          />
+        </div>
+        <div className={"w-full h-10 md:h-28  md:flex"} />
+      </div>
+
+      {/*  image container   */}
+      <div className={"flex-1 center pt-0 md:pt-10 relative h-auto md:h-[90%]"}>
+        <img
+          className={
+            " object-contain absolute ml-6 -left-5 -right-5 md:relative md:w-[75%]"
+          }
+          src="/images/general/bg-logo.png"
+          alt="bg_logo"
+        />
+        <img
+          src="/images/home/main-1.png"
+          alt="bg_logo"
+          className={"absolute inset-0 h-full  object-contain"}
+        />
+      </div>
+    </SectionWrapper>
+  );
 };
 
 export default Main;

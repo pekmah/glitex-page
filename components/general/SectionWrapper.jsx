800d760b9b34bf5d1c7b79f05901f767a318cd73
Pekmah <ericpekmah@gmail.com>
2023-12-10 11:07:47 +0300

    feat(#20): section wrapper added

--- components/general/SectionWrapper.jsx
+++ components/general/SectionWrapper.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const SectionWrapper = (className, sectionClassName, children) => {
+const SectionWrapper = ({ className, sectionClassName, children }) => {
   return (
     <section className={`center ${sectionClassName}`}>
-      <div className={`lg:container ${className}`}>{children}</div>
+      <div className={`w-full lg:container ${className}`}>{children}</div>
     </section>
   );
 };
--- components/general/index.js
+++ components/general/index.js
@@ -2,5 +2,6 @@ import CButton from "./CButton";
 import CButtonOutlined from "./CButtonOutlined";
 import SideNav from "./SideNav";
 import WhatsappBtn from "./WhatsappBtn";
+import SectionWrapper from "./SectionWrapper";
 
-export { CButton, CButtonOutlined, SideNav, WhatsappBtn };
+export { CButton, CButtonOutlined, SideNav, WhatsappBtn, SectionWrapper };

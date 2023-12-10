1ccf611aaf8c758d583561bdb16c3934402b6769
Pekmah <ericpekmah@gmail.com>
2023-12-10 12:03:51 +0300

    style(#20): btn class added, button shadows added

--- components/general/CButton.jsx
+++ components/general/CButton.jsx
@@ -2,11 +2,7 @@ import React from "react";
 
 const CButton = ({ text, handleClick, className, ...rest }) => {
   return (
-    <button
-      className={`bg-primary px-4 py-2.5 rounded-full text-white font-dm-sans font-bold text-sm ${className}`}
-      onClick={handleClick}
-      {...rest}
-    >
+    <button className={`btn ${className}`} onClick={handleClick} {...rest}>
       {text}
     </button>
   );

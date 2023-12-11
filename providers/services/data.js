4f50abc9582364d62c70bd1ef6d33d0dbfd4fe3b
Pekmah <ericpekmah@gmail.com>
2023-12-11 10:49:11 +0300

    feat(#22): services list added

--- providers/services/data.js
+++ providers/services/data.js
@@ -1,3 +1,48 @@
 export const services = [
+  {
+    title: "Search Engine Optimization",
+    desc: `<p>We are the leading search engine optimization company in Kenya. With 10+ years experience in search engine optimization, we have developed strategies to help our clients get more website visitors translating into more sales and revenue. Glitex equips you with top SEO strategies to ensure your business is easily discoverable by prospect clients.<p/>`,
+  },
+  {
+    title: "Mobile Applications",
+    desc: `<p>Discover the leading Mobile App Development Company in Nairobi, Kenya - Glitex Solutions Limited. Our proficient team specializes in Android and iOS mobile app development, delivering top-tier solutions at competitive prices. Whether you're based in Nairobi, Kenya, Ethiopia, Somalia, Rwanda, or Tanzania, we are dedicated to serving your mobile application needs.</p>`,
+  },
+  {
+    title: "Management Information Systems",
+    desc: `<div>
+    We pride ourselves in delivering end to end Information systems , ranging form:
+    <ul>
+      <li>1. Healthcare management systems</li>
+      <li>2. School management systems</li>
+      <li>3. Sacco management systems</li>
+      <li>4. ERP systems for both SMEs and Micro businesses.</li>
+      <li>5. Insurance Management systems. </li>
+     </ul>
+    </div>
+`,
+  },
+  {
+    title: "AI & ML Development",
+    desc: `<p>Glitex helps small to large enterprises develop smart and intelligent products leveraging deep learning and machine learning technology. Our team is well versed with AI and ML tool kits.</p>`,
+  },
 
-]
+  {
+    title: "E-Learning platform",
+    desc: `<p>Glitex is well versed with designing and developing technology-based interactive learning solutions.Providing learning technologies , Including custome e-learning , mulit-device leaning, mobile learning and consulting for interactive e-learning curriculum design.</p>`,
+  },
+  {
+    title: "Game Development",
+    desc: `
+    <p>
+      Together we lead your project through all stages from game design to post-release support, using the extensive experience and creativity of our experts.
+      <br/>
+      <strong>PC</strong><br/>
+      We design and develop end-to-end games for personal computers with porting to windows and MacOS.
+      <br/>
+      <strong >Mobile</strong>
+        <br/>
+      We create games for iOS and android plaftorms or cross-platform development.
+    </p>
+    `,
+  },
+];

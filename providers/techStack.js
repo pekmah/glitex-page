e964a940d1d821739b1917236b596a8accfd277c
Pekmah <ericpekmah@gmail.com>
2023-12-12 10:02:38 +0300

    feat(#35): tech stack list created

---
+++ providers/techStack.js
@@ -0,0 +1,11 @@
+export const LANGUAGES = [
+  "/images/home/languages/flutter.png",
+  "/images/home/languages/golang.png",
+  "/images/home/languages/js.png",
+  "/images/home/languages/kotlin.png",
+  "/images/home/languages/node.png",
+  "/images/home/languages/python.png",
+  "/images/home/languages/react.png",
+  "/images/home/languages/solidity.png",
+  "/images/home/languages/spring.png",
+];

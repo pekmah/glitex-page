77d8c23b8b3b1077f6911d44857a977796404444
Pekmah <ericpekmah.com>
2023-12-25 10:41:26 -0800

    refactor(#44): small screen floater size decreased

--- components/general/RoundedBgFloater.jsx
+++ components/general/RoundedBgFloater.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const RoundedBgFloater = ({ className }) => {
   return (
     <div
-      className={`rounded-full h-48 md:h-72 w-48 md:w-72 bg-bg-yellow absolute z-0 inset-0 md:-mt-8 ${className}`}
+      className={`rounded-full h-32 md:h-72 w-32 md:w-72 bg-bg-yellow absolute z-0 inset-0 md:-mt-8 ${className}`}
     />
   );
 };

2265db7cb4bbd1233668d6fbe3541507e9333c2a
Pekmah <ericpekmah.com>
2023-12-26 19:13:08 +0300

    feat(#47): slider mobile section added

--- components/mobile/index.js
+++ components/mobile/index.js
@@ -2,5 +2,6 @@ import MainMobile from "./Main.jsx";
 import AboutMobile from "./About.jsx";
 import PortfolioMobile from "./Portfolio.jsx";
 import MobileTypes from "./Types.jsx";
+import SliderMobile from "./slider/index.jsx";
 
-export { MainMobile, AboutMobile, PortfolioMobile, MobileTypes };
+export { MainMobile, AboutMobile, PortfolioMobile, MobileTypes, SliderMobile };

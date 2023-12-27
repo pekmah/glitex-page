59aedc6a106cc14133f89ebc5d8b4befcd92ef18
Pekmah <ericpekmah.com>
2023-12-27 09:36:31 +0300

    feat(#58): steps added

---
+++ components/onboarding/steps/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { SectionWrapper } from "../../general";
+import Card from "./Card";
+import { ONBOARDING_STEPS } from "../../../providers/onboarding/data";
+
+const Steps = () => {
+  return (
+    <SectionWrapper
+      sectionClassName={"bg-white relative"}
+      className={"flex md:px-5 relative pb-12"}
+    >
+      <div
+        className={
+          "min-h-80 w-full -mt-32 md:-mt-40 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 md:gap-10 p-2"
+        }
+      >
+        {ONBOARDING_STEPS.map((step) => (
+          <Card
+            key={step.id}
+            index={step.id}
+            title={step.title}
+            description={step.description}
+          />
+        ))}
+      </div>
+    </SectionWrapper>
+  );
+};
+
+export default Steps;

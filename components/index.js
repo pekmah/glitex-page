41dee7d21c0a63d5a70204381d7bc169d2339e51
Pekmah <ericpekmah.com>
2023-12-27 08:39:47 +0300

    feat(#57): onboarding development section added

--- components/index.js
+++ components/index.js
@@ -2,3 +2,4 @@ export * from "./main";
 export * from "./general";
 export * from "./home";
 export * from "./mobile";
+export * from "./onboarding";
---
+++ components/onboarding/Development.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OnboardingDevelopment = () => {
+  return <div>Onboarding Development</div>;
+};
+
+export default OnboardingDevelopment;

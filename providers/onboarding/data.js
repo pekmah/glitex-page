748bdfaf862bc6fe71f11717e9907ed57b444ded
Pekmah <ericpekmah.com>
2023-12-27 09:06:19 +0300

    feat(#58): onBoarding steps added

---
+++ providers/onboarding/data.js
@@ -0,0 +1,73 @@
+export const ONBOARDING_STEPS = [
+  {
+    id: 1,
+    title: "Introduction",
+    description:
+      "This is the stage where we receive a call or an enquiry from a prospective client from our various marketing channels. during this phase we introduce ourselves and answer any questions that the prospect might have , If the prospect is still interested we organize to schedule for a project briefing session.",
+  },
+  {
+    id: 2,
+    title: "NDA Signing",
+    description:
+      "Glitex legitimately has an NDA that we avail for our clients, If our clients have theirs , we review it with our prominent lawyers , come to an agreement and sign it off. This ensures we have a good working relation.",
+  },
+  {
+    id: 3,
+    title: "Project briefing/ Requirement workshop.",
+    description:
+      "This session is mainly run by our founders and senior developers as we get to understand if we are the right fit for the client needs. The client describes the problem or requirements as our team seeks to deeply understand.",
+  },
+  {
+    id: 4,
+    title: "Official proposal",
+    description:
+      "Glitex helps small to large enterprises develop smart and intelligent products leveraging deep learning and machine learning technology. Our team is well versed with AI and ML tool kits.",
+  },
+  {
+    id: 5,
+    title: "Research and Solution development",
+    description:
+      "After the session , we conduct a research to further our understanding. We then develop a solution that we believe serves the client's needs.",
+  },
+  {
+    id: 6,
+    title: "Negotiation and agreement",
+    description:
+      "At this stage our founders negotiate on the price and timelines. At this stage we expect both parties to come to an agreement.",
+  },
+  {
+    id: 7,
+    title: "Contract signing",
+    description:
+      "We rewrite the proposal again as per the agreed cost and timeline and sign it off as a binding contract.",
+  },
+  {
+    id: 8,
+    title: "Project Milestone Mapping",
+    description:
+      "The Chief product officer leads a session with the lead team handling the project and map out the key milestones with their subsequent timelines.",
+  },
+  {
+    id: 9,
+    title: "Project Kick-off and server setup",
+    description:
+      "The team starts off with the UX process as the engineers handle the system architecture design , set up environments.",
+  },
+  {
+    id: 10,
+    title: "Weekly updating",
+    description:
+      "The client and the team have weekly updates/ calls on the project progress.",
+  },
+  {
+    id: 11,
+    title: "Contract signing",
+    description:
+      "We rewrite the proposal again as per the agreed cost and timeline and sign it off as a binding contract.",
+  },
+  {
+    id: 12,
+    title: "Testing & QA",
+    description: "testing & QA will be promptly done during development.",
+  },
+];

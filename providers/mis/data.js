d90488126d14c19ff93258f42901edd48e3907e0
Pekmah <ericpekmah.com>
2024-01-01 22:42:52 +0300

    feat(#74): ai & SEO Data added

---
+++ providers/mis/data.js
@@ -0,0 +1,218 @@
+export const pages = {
+  //page
+  seo: {
+    //  main section
+    main: {
+      title: `
+          <h1 class={"md:leading-[60px] w-full md:w-4/5"}>
+              Search Engine Evaluator
+              <br class={"hidden md:flex"} /> Companies in Kenya |
+              <br class={"hidden md:flex"} /> SEO Services Nairobi
+              <br class={"hidden md:flex"} /> Kenya.
+          </h1>
+        `,
+      subtitle: null,
+      imageUrl: "/images/mis/seo-main.webp",
+    },
+    //     Description section
+    description: {
+      title: `
+          <h1 class={"h2 leading-8 md:leading-11"}>
+        Search Engine Evaluator
+        <br class={"hidden md:flex"} /> Companies in Kenya.
+      </h1>
+        `,
+      body: `
+      <p
+        class={
+          "w-full md:w-3/5 mx-auto my-2 md:my-5 text-base text-primary text-left md:text-center"
+        }
+      >
+        Are you searching for Search Engine evaluators companies in Kenya? We are undisputed Search Engine Optimization (SEO) Company in Nairobi, Kenya. Glitex Solutions Limited is the leading search engine optimization company in Kenya and across the region. This guarantees you of quality. Good ranking on all search engines means more website visitors, sales and revenue.
+        <br class={"hidden md:flex"} />
+        <br class={"hidden md:flex"} />
+        We use our SEO tactics and strategies to ensure you’re on ranked on top of the search engines. Search engine optimization (SEO) is a tested and proven method to grow your company by giving it visibility countrywide and across the region. A website without traffic doesn't serve its purpose but a website with the right traffic flowing in consistently can propel a business to new scales. Our SEO Team will analyze the markets and get to work making your website more visible to your key market through our comprehensive digital marketing services.
+      </p>
+      `,
+    },
+    //   About section
+    about: {
+      title: `
+          <h2 class={"leading-8 md:leading-11"}>
+       SEO Services in Kenya | Search Engine
+        <br class={"hidden md:flex"} /> Optimization Services in Kenya.
+      </h2>
+        `,
+      //   body
+      body: [
+        {
+          imageUrl: "/images/mis/seo-optimization.webp",
+          description:
+            "Are you searching for Search Engine evaluators companies in Kenya? We are undisputed Search Engine Optimization (SEO) Company in Nairobi, Kenya. Glitex Solutions Limited is the leading search engine optimization company in Kenya and across the region. This guarantees you of quality. Good ranking on all search engines means more website visitors, sales and revenue. We use our SEO tactics and strategies to ensure you’re on ranked on top of the search engines. Search engine optimization (SEO) is a tested and proven method to grow your company by giving it visibility countrywide and across the region. A website without traffic doesn't serve its purpose but a website with the right traffic flowing in consistently can propel a business to new scales. Our SEO Team will analyze the markets and get to work making your website more visible to your key market through our comprehensive digital marketing services.",
+        },
+      ],
+    },
+    //   Steps section(Benefits)
+    steps: {
+      title: `
+          <h2 class={"leading-8 md:leading-11"}>
+       Benefits of Search Engine Optimization (SEO).
+      </h2>
+        `,
+      subtitle: null,
+      steps: [
+        {
+          title: "Increase in Sales",
+          body: "The higher the number of quality and organic traffic on your website, the more the leads and definitely sales you will generate.",
+        },
+        {
+          title: "Website Traffic",
+          body: "SEO drive quality and organic traffic to your website. After we offer SEO services, the number of people visiting your website will increase.",
+        },
+        {
+          title: "Sustainable Results",
+          body: "Glitex Solutions will ensure that you maintain high Ranking on all search engines. We will maintain your tracking in search engines 24/7.",
+        },
+        {
+          title: "Brand Credibility",
+          body: "Ranking top on Google and other search engines will automatically make you look like the leader in your industry.",
+        },
+        {
+          title: "Our SEO Content Strategy",
+          body: `
+          <ul>
+            <li>1. Make a list of the products and services.</li>
+            <li>2. Develop a list of keywords related to your products and services.</li>
+            <li>3. Develop pages for each product</li>
+            <li>4. Develop articles for those pages.</li>
+            <li>5. Develop a link-building plan.</li>
+            <li>6. Compressing media to SEO friendly sizes</li>
+          </ul>
+          `,
+        },
+      ],
+    },
+    //   Floater section.
+    floater: {
+      title: `<h2
+        class={" leading-8 md:leading-11 mb-10 md:bg-24 text-center mt-12"}
+      >
+        We will always show up <br class={"hidden md:block"} />
+        in your business
+      </h2>`,
+      body: `
+        <p class={"my-10 md:w-1/2 mx-auto text-center"}>
+          Working with the best SEO Company in Nairobi and in Kenya can always keep 
+          <br class={"hidden md:block"} />
+          your business on top of all search engines.
+        </p>
+      `,
+    },
+  },
+  ai: {
+    //  main section
+    main: {
+      title: `
+          <h1 class={"md:leading-[60px] w-full md:w-4/5"}>
+              AI and ML (Artifical
+              <br class={"hidden md:flex"} /> Intelligence &
+              <br class={"hidden md:flex"} /> Machine learning)
+              <br class={"hidden md:flex"} /> Developers in
+              <br class={"hidden md:flex"} /> Nairobi Kenya.
+          </h1>
+        `,
+      subtitle: null,
+      imageUrl: "/images/mis/ai-main.webp",
+    },
+    //     Description section
+    description: {
+      title: `
+          <h1 class={"h2 leading-8 md:leading-11"}>
+        AI Companies in Kenya.
+      </h1>
+        `,
+      body: `
+      <p
+        class={
+          "w-full md:w-3/5 mx-auto my-2 md:my-5 text-base text-primary text-left md:text-center"
+        } 
+      >
+        Glitex Solutins Limited is an AI and ML (Artificial intelligence & Machine learning) Software development
+        <br class={"hidden md:flex"} />
+        company based in Nairobi Kenya. We have a skilled team that focuses on delivering quality.
+      </p>
+      `,
+    },
+    //   About section
+    about: {
+      title: null,
+      //   body
+      body: [
+        {
+          imageUrl: "/images/mis/ai-about-1.webp",
+          description: {
+            title: "Artificial Intelligence",
+            body: "Our Artificial Intelligence Software Solutions are revolutionizing businesses globally. Harness the power of AI to uncover new opportunities for growth, tackle intricate issues, and drive innovation within your organization.",
+          },
+        },
+        {
+          imageUrl: "/images/mis/ai-about-2.webp",
+          description: {
+            title: "Machine Learning",
+            body: "At Glitex Solutions Limited, we leverage the power of advanced machine learning to unlock the full potential of digital data. Our cutting-edge algorithms and techniques are utilized to deliver accurate predictions, detect anomalies, and address a wide range of business challenges. By utilizing our ML solutions, our clients can gain a competitive edge, explore new business avenues, and achieve exceptional performance levels.",
+          },
+        },
+      ],
+    },
+    //   Steps section(Benefits)
+    steps: {
+      title: `
+          <h2 class={"leading-8 md:leading-11"}>
+       Our focus on Artificial Intelligence
+       <br class={"hidden md:flex"} />
+       and machine learning.
+      </h2>
+        `,
+      subtitle: null,
+      steps: [
+        {
+          title: "Ai driven software apps",
+          body: "Our AI powered software application utilizes advanced artificial intelligence techniques and algorithms to enhance performance and deliver innovative solutions. With its ability to process vast amounts of digital data, this AI software can provide valuable insights, automate processes, and drive new business opportunities for organizations.",
+        },
+        {
+          title: "Ecommerce portals",
+          body: "Glitex Solutions harnesses the power of AI to develop self-learning e-Commerce applications. Our data-driven solutions aim to give e-Commerce platforms an edge by offering accurate predictions, cross-selling recommendations, automated pricing, and product suggestions.",
+        },
+        {
+          title: "Smart business process",
+          body: "Utilizing AI and Machine learning, conventional processes that were based on rules are now being transformed into intelligent ones that have the ability to uncover new patterns in vast and complex data sets. These intelligent processes make strategic predictions and recommendations without the need for explicit directions. With the use of AI, many repetitive tasks are being replaced by smart automation, demonstrating its credibility as a leading alternative solution.",
+        },
+        {
+          title: "Digital Assistants & Self learning.",
+          body: "Our AI-powered software endows digital assistants and enterprise software with self-learning abilities. These programs have the ability to analyze key events and contexts and provide recommendations based on specified parameters. Tasks such as prioritizing the day, managing appointments, handling documents, reminders and project management can now all be efficiently executed through the use of digital assistants.",
+        },
+        {
+          title: "Real-time Data Science",
+          body: `
+          The vastness of digital data can be overwhelming and it's crucial for companies to have real-time access to it in order to remain competitive. Our utilization of Machine Learning enables us to uncover the underlying patterns in the data that are relevant to significant business events. This is achieved by analyzing data from a multitude of sources and delivering insightful results.
+          `,
+        },
+      ],
+    },
+    //   Floater section.
+    floater: {
+      title: `<h2
+        class={" leading-8 md:leading-11 mb-10 md:bg-24 text-center mt-12"}
+      >
+       Benefits of AI & Machine Learning
+      </h2>`,
+      body: `
+        <p class={"my-10 md:w-1/2 mx-auto text-center"}>
+          Working with the best SEO Company in Nairobi and in Kenya can always keep 
+          <br class={"hidden md:block"} />
+          your business on top of all search engines.
+        </p>
+      `,
+    },
+  },
+};

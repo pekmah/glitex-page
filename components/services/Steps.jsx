5d716e807b394a317dda170c060d459857822de4
Pekmah <ericpekmah.com>
2024-01-03 09:37:46 +0300

    feat(#75): service floater added

--- components/services/Steps.jsx
+++ components/services/Steps.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import {SectionWrapper} from "../general";
 import Card from "../onboarding/steps/Card";
+import {ServiceFloater} from "./index";
 
 /**
  * renders procedures
- * @param {{title:string,subtitle:string,steps:Object[]}} props
+ * @param {{title:string,subtitle:string,steps:Object[],floater:Object}} props
  * @returns {JSX.Element}
  * @constructor
  */
 const Steps = (props) => {
   return (
     <SectionWrapper
-      sectionClassName={"bg-bg-blue min-h-90"}
+      sectionClassName={"bg-bg-blue min-h-90 relative"}
       className={"py-10 md:py-24 px-5 md:px-0"}
     >
       <h2 className={"leading-8 md:leading-11 md:w-1/2 mx-auto mb-5"}>
@@ -31,7 +32,7 @@ const Steps = (props) => {
           "min-h-80 w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 md:gap-10 p-2"
         }
       >
-        {props?.steps.map((step, ind) => (
+        {props?.steps?.map((step, ind) => (
           <Card
             key={ind}
             index={ind + 1}
@@ -40,6 +41,13 @@ const Steps = (props) => {
           />
         ))}
       </div>
+
+      {/*  floater section  */}
+      <ServiceFloater
+        title={props?.floater?.title}
+        body={props?.floater?.body}
+        bottom={props?.floater?.style?.bottom}
+      />
     </SectionWrapper>
   );
 };
--- components/services/index.js
+++ components/services/index.js
@@ -2,5 +2,12 @@ import ServiceMain from "./Main";
 import ServiceDescription from "./Description";
 import ServiceAbout from "./About";
 import ServiceSteps from "./Steps";
+import ServiceFloater from "./Floater";
 
-export { ServiceMain, ServiceDescription, ServiceAbout, ServiceSteps };
+export {
+  ServiceMain,
+  ServiceDescription,
+  ServiceAbout,
+  ServiceSteps,
+  ServiceFloater,
+};

02e2d99a89b47555c445e174fba46dba4b6233b0
Pekmah <ericpekmah.com>
2024-01-05 08:27:24 +0300

    feat(#82): our purpose section added

--- components/low_code/OurPurpose.jsx
+++ components/low_code/OurPurpose.jsx
@@ -1,8 +1,47 @@
 import React from "react";
-import Layout from "../../layout";
+
+import { SectionWrapper } from "../general";
+import { FOUNDER } from "../../constants/defaults";
 
 const OurPurpose = () => {
-  return <Layout>Purpose section</Layout>;
+  return (
+    <SectionWrapper
+      sectionClassName={"bg-white relative"}
+      className={"md:px-5 relative"}
+    >
+      <div className="h-32 md:h-[380px] w-full" />
+
+      <img
+        src="/images/low_code/our_purpose-1.webp"
+        alt="low code"
+        className={
+          "absolute mx-auto inset-x-0 -top-28 md:-top-60 object-contain w-11/12 md:w-8/12"
+        }
+      />
+
+      <h1 className={"h2 leading-8 md:leading-11 md:w-1/2 mx-auto text-center"}>
+        Our Purpose
+      </h1>
+
+      <p
+        className={
+          "w-full md:w-1/2 mx-auto my-2 md:my-5 text-base text-primary text-left md:text-center"
+        }
+      >
+        &quot;We exist to help you build a market-ready product{" "}
+        <br className={"hidden md:flex"} /> fastest and with least
+        expenses&quot;
+      </p>
+
+      <p
+        className={
+          "w-full md:w-1/2 mx-auto my-2 md:my-5 text-base text-primary text-left md:text-center"
+        }
+      >
+        ~ {FOUNDER}
+      </p>
+    </SectionWrapper>
+  );
 };
 
 export default OurPurpose;

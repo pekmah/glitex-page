1cc2d71e008cf184769706b10dc35ede96311c22
Pekmah <ericpekmah.com>
2024-01-07 14:30:59 +0300

    style(#92): heading text color converted to primary

--- components/home/WhatWeDo.jsx
+++ components/home/WhatWeDo.jsx
@@ -8,21 +8,21 @@ const WhatWeDo = () => {
       sectionClassName={"bg-white "}
       className={"py-16 md:py-32 px-5 md:px-0 text-center"}
     >
-      <h2 className={"leading-8 md:leading-11 text-secondary"}>What we do</h2>
-      <h3
+      <h2 className={"leading-8 md:leading-11 text-primary"}>What we do</h2>
+      <h2
         className={
-          "text-center leading-7 md:leading-10 font-normal my-0 md:my-2"
+          "text-center leading-7 md:leading-10 font-normal my-0 md:my-2 "
         }
       >
         We’ve got the skills and workforce you can rely{" "}
         <br className={"hidden md:block"} /> on to push your business to the
         next level.
-      </h3>
+      </h2>
 
       {/*  services grid  */}
       <div
         className={
-          "my-4 md:my-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10"
+          "my-10 md:my-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10"
         }
       >
         {services.map((service, ind) => (

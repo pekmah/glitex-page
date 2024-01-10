73129e04ae2cd0abbd5002a07b285f15f5df359e
Pekmah <ericpekmah.com>
2024-01-10 08:11:49 +0300

    feat: social links added

--- constants/defaults.js
+++ constants/defaults.js
@@ -1,2 +1,8 @@
 export const FOUNDER = "Peter Njenga";
 export const CONTACT_PHONE_NUMBER = "+254707021821";
+export const SOCIAL_LINKS = {
+  facebook: "https://web.facebook.com/glitexsolutionslimited",
+  instagram: "https://www.instagram.com/glitexsolutions/",
+  x: "https://twitter.com/GlitexSolutions",
+  linkedin: "https://www.linkedin.com/company/glitex-solutions-limited/",
+};

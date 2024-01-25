99d89aa7da4060c6e1eeb669274f2ffdbc65888c
Pekmah <ericpekmah.com>
2024-01-25 21:47:56 +0300

    refactor: disable esling

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,9 +11,11 @@ class MyDocument extends Document {
           <link rel="stylesheet" href="/css/slider.css" />
           <script type="text/javascript">
               (function(c,l,a,r,i,t,y){
+                  // eslint-disable-next-line
                   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                  // eslint-disable-next-line
               })(window, document, "clarity", "script", "kjoq58b3y8");
           </script>
 

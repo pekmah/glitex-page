948a84713abf8e7ab881f4518f3304a8fea62e4d
Pekmah <ericpekmah.com>
2024-01-26 18:10:10 +0300

    feat: png images converted to webp

--- providers/customers.js
+++ providers/customers.js
@@ -1,10 +1,10 @@
 export const CUSTOMERS = [
-  "/images/customers/kengen.png",
-  "/images/customers/muthaiga.png",
-  "/images/customers/naboisho.png",
-  "/images/customers/nema.png",
-  "/images/customers/nyeri-county.png",
-  "/images/customers/okapy.png",
-  "/images/customers/safaricom.png",
-  "/images/customers/world-bank.png",
+  "/images/customers/kengen.webp",
+  "/images/customers/muthaiga.webp",
+  "/images/customers/naboisho.webp",
+  "/images/customers/nema.webp",
+  "/images/customers/nyeri-county.webp",
+  "/images/customers/okapy.webp",
+  "/images/customers/safaricom.webp",
+  "/images/customers/world-bank.webp",
 ];

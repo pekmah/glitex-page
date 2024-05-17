9b0ddf0ff681d163cc475888435cd196e4d50d4d
pekmah <51937220+pekmah@users.noreply.github.com>
2024-05-17 13:20:11 +0300

    feat: Tawk ai chat script added

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -25,6 +25,22 @@ class MyDocument extends Document {
             `,
             }}
           />
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/66470d1d981b6c56477177ae/1hu2omf8f';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+            `,
+            }}
+          />
         </Head>
         <body id="top">
           <Main />

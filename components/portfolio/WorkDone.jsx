f1d51cfdd856d6da32d651963cf32167d2187ad8
pekmah <51937220+pekmah@users.noreply.github.com>
2024-08-15 08:34:19 +0300

    refactor: swiper css imports deleted

--- components/portfolio/WorkDone.jsx
+++ components/portfolio/WorkDone.jsx
@@ -1,15 +1,10 @@
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
 import React from "react";
 import SectionWrapper from "../general/SectionWrapper";
 
 const WorkDone = () => {
   return (
     <SectionWrapper
-      sectionClassName={"relative bg-bg-yellow pb-20 -mt-10 min-h-screen"}
+      sectionClassName={"relative bg-white pb-20 -mt-10 min-h-screen"}
       className={"py-10 md:py-3 pt-20 md:pt-0 px-5 md:px-0 "}
     >
       {/* */}

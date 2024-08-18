dd4c81213a43205657a5391dd9a7ebb7deb9aa7b
pekmah <51937220+pekmah@users.noreply.github.com>
2024-08-18 12:08:47 +0300

    feat: book appointment script added

--- pages/book.jsx
+++ pages/book.jsx
@@ -1,25 +1,36 @@
+import React, { useEffect } from "react";
+
 import Layout from "../layout";
-import React from "react";
 import { SectionWrapper } from "../components";
 
 const Mobile = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://assets.calendly.com/assets/external/widget.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <Layout>
       <SectionWrapper
         sectionClassName={"h-[75vh] md:-bottom-[400px] mx-3"}
         className={" p-5 md:p-8 rounded-2xl "}
       >
-        {/* <div className={"center"}> */}
         <div
           className="calendly-inline-widget"
           data-url="https://calendly.com/glitexsolutions-info/30min"
-          //   style="min-width:320px;height:700px;"
+          style={{
+            minWidth: "320px",
+            width: "100%",
+            minHeight: "400px",
+            height: "80vh",
+          }}
         ></div>
-        <script
-          type="text/javascript"
-          src="https://assets.calendly.com/assets/external/widget.js"
-          async
-        ></script>
       </SectionWrapper>
     </Layout>
   );

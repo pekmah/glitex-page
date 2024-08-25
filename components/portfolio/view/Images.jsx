8186f3422206bf522c6bb6255a5b6ec5c4be919c
pekmah <51937220+pekmah@users.noreply.github.com>
2024-08-25 15:14:54 +0300

    feat: tab to switch mobile admin profile images added

--- components/portfolio/view/Images.jsx
+++ components/portfolio/view/Images.jsx
@@ -1,18 +1,19 @@
 import React, { useMemo } from "react";
+
 import { SectionWrapper } from "../../general";
 import { portfolioData } from "../../../providers/projects/data";
 import { useParams } from "next/navigation";
 
-const Images = () => {
+const Images = ({ tab }) => {
   const params = useParams();
 
   const currentProject = useMemo(() => {
     if (params?.id) {
-      return portfolioData[params?.id];
+      return portfolioData[params?.id]?.images[tab];
     } else {
       return null;
     }
-  }, [params?.id]);
+  }, [params?.id, tab]);
 
   return (
     <SectionWrapper
@@ -21,11 +22,11 @@ const Images = () => {
         "grid gap-5 md:gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 pb-14 md:pb-32 "
       }
     >
-      {currentProject?.images?.map((img, ind) => (
+      {currentProject?.map((img, ind) => (
         <img
           key={ind}
           src={img}
-          className={"object-contain w-full c-shadow rounded-2xl lg:h-[720px]"}
+          className={"object-contain w-full rounded-2xl lg:h-[720px]"}
           alt={img}
         />
       ))}

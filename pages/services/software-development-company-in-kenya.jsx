d9ec17c42ea13dd40b871fcc4a4038e1275ee153
pekmah <51937220+pekmah@users.noreply.github.com>
2024-08-26 09:14:31 +0300

    feat: updated software-dev-page meta

--- pages/services/software-development-company-in-kenya.jsx
+++ pages/services/software-development-company-in-kenya.jsx
@@ -10,38 +10,32 @@ import Head from "next/head";
 import Layout from "../../layout";
 import React from "react";
 
+// meta data
+const CONTENT =
+  "Are you in search of the top software development company in Kenya and beyond? Your search ends here. Glitex Solutions Limited is the pinnacle of innovation, excellence, and client satisfaction. Based in Nairobi, we are at the forefront of technology, providing state-of-the-art software development services that elevate businesses to new levels of success.";
+const META_TITLE =
+  "Software development company in Kenya | Tech company in Kenya";
+
 const Mobile = () => {
   return (
     <Layout>
       <Head>
-        <title>
-          Mobile App Developers in Kenya | Android Apps | IOS Apps | Glitex
-          Solutions Limited
-        </title>
+        <title>{META_TITLE}</title>
         <meta name="robots" content="index, follow" />
-        <meta
-          name="description"
-          content="For Android mobile app and IOS mobile application development in Kenya, Contact Glitex Solutions Limited. We have the best mobile app developers in Nairobi Kenya."
-        />
+        <meta name="description" content={CONTENT} />
         <meta
           name="keywords"
-          content="Mobile App developers in Kenya, IOS &amp; Android mobile application development in Kenya, Mobile app development company in Kenya"
+          content="Software development company in kenya, tech companies in kenya, tech company in kenya"
         />
         <link
           rel="canonical"
-          href="https://glitexsolutions.co.ke/services/mobile-app-developers-in-kenya-1"
+          href="https://www.glitexsolutions.co.ke/services/software-development-company-in-kenya"
         />
         <meta property="og:site_name" content="Glitex Solutions Limited" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://glitexsolutions.co.ke/" />
-        <meta
-          property="og:title"
-          content="Mobile App Developers in Kenya | Android Apps | IOS Apps"
-        />
-        <meta
-          property="og:description"
-          content="For Android mobile app and IOS mobile application development in Kenya, Contact Glitex Solutions Limited. We have the best mobile app developers in Nairobi Kenya."
-        />
+        <meta property="og:title" content={META_TITLE} />
+        <meta property="og:description" content={CONTENT} />
         <meta name="next-head-count" content="14" />
       </Head>
 

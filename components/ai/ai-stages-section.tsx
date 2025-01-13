ad4257bd92cdd60c8dcecdf8c312c13be53c125a
Pekstaar <pekstaar@gmail.com>
2025-01-13 19:52:11 +0300

    feat: ai-stages section made responsive

--- components/ai/ai-stages-section.tsx
+++ components/ai/ai-stages-section.tsx
@@ -4,16 +4,16 @@ import { SectionWrapper } from "../general";
 export default function AiStagesSection() {
   return (
     <SectionWrapper
-      className={"lg:px-10"}
+      className={"px-3 lg:px-10"}
       sectionClassName={"bg-[#002345]"}
       sectionStyles={undefined}
     >
-      <div className="bg-[url('/images/ai/ai-stages-bg.jpg')] bg-contain bg-center bg-no-repeat py-20 ">
+      <div className="bg-[url('/images/ai/ai-stages-bg.jpg')] bg-contain bg-center bg-no-repeat py-16 sm:py-20 ">
         <h2 className={"leading-8 md:leading-11 text-white"}>
-          What does this mean for your startup
+          What does this mean for your startup?
         </h2>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 md:gap-10 p-2 pt-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 md:gap-10 p-2 pt-10 md:pt-20">
           {stages.map((stage, ind) => (
             <Card
               key={ind}
@@ -35,9 +35,11 @@ const Card = ({
   description: string;
 }) => (
   <div className="bg-white p-7 rounded-[40px] border border-gray-200 min-h-48">
-    <h3 className="text-2xl font-bold font-tinos my-3 mb-8">{title}</h3>
+    <h3 className="text-xl sm:text-xl md:text-2xl font-bold font-tinos my-3 md:mb-8">
+      {title}
+    </h3>
 
-    <p className="text-base text-primary mr-6">{description}</p>
+    <p className="text-sm sm:text-base text-primary mr-6">{description}</p>
   </div>
 );
 

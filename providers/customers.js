d91034fb0323a181a6fd3ff075edd43ac1ecd9b2
Eric Pekmah <ericpekmah@gmail.com>
2025-02-06 10:37:25 +0300

    feat: customer images updated(kra & rmf)

--- providers/customers.js
+++ providers/customers.js
@@ -1,8 +1,8 @@
 export const CUSTOMERS = [
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTcWE7YGjm9PUVjJADuSiTVDP-AgV8OnaIqg&s",
+  "/images/customers/kra.png",
   "/images/customers/kengen.webp",
   "/images/customers/muthaiga.webp",
-  "https://play-lh.googleusercontent.com/kGkyKJYCGssj06bp6xI3z0zHFsLsXQPLcqO-qkh9yxVn0qVcbV2PF_GWdmtJqLfRNHI",
+  "/images/customers/rmf.png",
   "/images/customers/world-bank.webp",
   "https://play-lh.googleusercontent.com/c5ziFnxH2QG4m25tI4ombExZr8P7nK937SNtuD01b_YnRi-M_xkenSA9H3i217VFpQ",
   "/images/customers/nema.webp",
